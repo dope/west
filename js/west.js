7082019a7afc0706d3bbe90aced9831f7007b051
Dope <hello.joer@gmail.com>
2013-08-06 00:54:31 +0100

    updated v1.5

---
+++ js/west.js
@@ -0,0 +1,53 @@
+
+$(function () {
+    var menuStatus;
+
+    $(".nav-btn").click(function() {
+        if (menuStatus != true) {
+            $(".slideWrap").show();
+            $(".slideWrap, body").animate({
+                marginLeft: "250px",
+            }, 100, function () {
+                menuStatus = true;
+            });
+            return false;
+        } else {
+            $(".slideWrap, body").animate({
+                marginLeft: "0px",
+            }, 100, function () {
+                menuStatus = false;
+                $(".slideWrap").hide();
+            });
+            return false;
+        }
+
+    });
+
+    $('html').live("swipeleft", function () {
+        if (menuStatus) {
+            $("body, .slideWrap").animate({
+                marginLeft: "0px",
+            }, 0, function () {
+                menuStatus = false;
+                $(".slideWrap").hide();
+            });
+        }
+    });
+
+    $('html').live("swiperight", function () {
+        if (!menuStatus) {
+            $(".slideWrap").show();
+            $("body, .slideWrap").animate({
+                marginLeft: "250px",
+            }, 0, function () {
+                menuStatus = true;
+            });
+        }
+    });
+
+    $('.nav-btn, a').bind('touchstart', function (e) {
+        $(this).trigger('click');
+        e.preventDefault();
+    });
+
+});

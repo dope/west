ec1f69d5f79915847ba632ee6bb79a8855fce797
Joe <hello.joer@gmail.com>
2013-12-04 22:58:17 +0000

    updated with a better navigation structure

--- js/west.js
+++ js/west.js
@@ -1,29 +1,13 @@
-$(function () {
+ $(function () {
 
     $(".trigger").click(function () {
         $('.navWrap').toggleClass('open');
+        $('body').toggleClass('bodyLeft');
     });
 
-    $('html').on("swiperight", function () {
-        $('.navWrap').removeClass('open');
-    });
-
-    $('html').on("swipeleft", function () {
-        $('.navWrap').addClass('open');
-    });
-
-    $( ".trigger" ).draggable();
 
     $('.trigger').bind('touchstart', function (e) {
         $(this).trigger('click');
         e.preventDefault();
     });
-
-    $.mobile.loading('show', {
-        theme: "b",
-        text: "",
-        textonly: false
-    });
-
 });
-

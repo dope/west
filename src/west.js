6e0a74acb3ddd9d3540e26aa7980305f908b6eda
Joe Richardson <joe@lifes.gd>
2016-05-29 13:31:44 +0100

    Cleaned up Javascript

--- src/west.js
+++ src/west.js
@@ -1,13 +1,34 @@
+var selectors = {
+    offScreen:      'off-screen',
+    onScreen:       'on-screen',
+    triggerActive:  'west__trigger--active'
+}
+
 var westTrigger   = document.querySelector('.west__trigger');
 var westContainer = document.documentElement;
 
 // Add off-screen to HTML by default
-westContainer.classList.add('off-screen')
+westContainer.classList.add(selectors.offScreen)
 
 // Toggle menu
-function toggleMenu() {
-    westContainer.classList.toggle('on-screen');
-    westTrigger.classList.toggle('west__trigger--active');
+function closeMenu() {
+    if (westContainer.classList.contains(selectors.onScreen)) {
+        westContainer.classList.remove(selectors.onScreen);
+        westTrigger.classList.remove(selectors.triggerActive);
+    } else {
+        westContainer.classList.add(selectors.onScreen);
+        westTrigger.classList.add(selectors.triggerActive);
+    }
 }
 
-westTrigger.addEventListener('click', toggleMenu);
+westTrigger.addEventListener('click', closeMenu);
+
+// Close menu with Escape Key
+document.onkeydown = function(event) {
+    event = event || window.event;
+    if (event.keyCode == 27) {
+        if (westContainer.classList.contains(selectors.onScreen)) {
+            closeMenu();
+        }
+    }
+};
